7e5906247022d364e219e017093c09464f55217b
Zzzzzzy123 <1274975655@qq.com>
2023-12-08 15:10:27 +0800

    update

--- src/main/java/com/touchfish/MiddleClass/DisplayInfo.java
+++ src/main/java/com/touchfish/MiddleClass/DisplayInfo.java
@@ -1,8 +1,10 @@
 package com.touchfish.MiddleClass;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class DisplayInfo {
     public String id;
     public String display_name;
