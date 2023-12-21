110467e07c96745952cace499b157aa2293705c9
sunman2003 <6489366@qq.com>
2023-12-21 14:37:39 +0800

    12/21 saved

--- src/main/java/com/touchfish/Controller/AdminController.java
+++ src/main/java/com/touchfish/Controller/AdminController.java
@@ -11,6 +11,7 @@ import com.touchfish.Service.impl.NoticeImpl;
 import com.touchfish.Service.impl.UserImpl;
 import com.touchfish.Tool.LoginCheck;
 import com.touchfish.Tool.Result;
+import com.touchfish.Tool.UserContext;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
@@ -39,10 +39,11 @@ public class AdminController {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return formatter.format(date);
     }
+
     @GetMapping ("/unclaimed")
     @LoginCheck
     @Operation(summary = "获取未处理的认领申请")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "浏览的文献id")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "不需要")
     public Result<List<ClaimRequest>> getUnhandledClaim(){
         List<ClaimRequest> unClaimedList = claimRequest.lambdaQuery().eq(ClaimRequest::getStatus, 0).list();
         return Result.ok("查询所有未处理认领门户申请成功", unClaimedList);
@@ -51,7 +52,7 @@ public class AdminController {
     @PostMapping ("/handle/claim")
     @LoginCheck
     @Operation(summary = "处理认领申请")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "浏览的文献id")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "认领申请id 处理结果(false表示不通过，true为通过)")
     public Result<String> handleClaimRequest(@RequestBody ClaimResultInfo claimResultInfo){
         ClaimRequest targetClaim = claimRequest.getBaseMapper().selectById(claimResultInfo.getClaimRequestId());
 
@@ -92,5 +93,15 @@ public class AdminController {
         return Result.ok("处理认领申请成功");
     }
 
-    
+    @GetMapping ("/myclaims")
+    @LoginCheck
+    @Operation(summary = "获取当前管理员处理的认领申请")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "不需要")
+    public Result<List<ClaimRequest>> getAdminClaim(){
+        List<ClaimRequest> ClaimedList = claimRequest.lambdaQuery().eq(ClaimRequest::getHandler_id,
+                UserContext.getUser().getUid()).list();
+        return Result.ok("查询管理员处理的认领门户申请成功", ClaimedList);
+    }
+
+
 }
