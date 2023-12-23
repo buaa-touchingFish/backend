3bee7c975b8c6516abecbd5948ac6a0b9670b316
Zzzzzzy123 <1274975655@qq.com>
2023-12-23 17:13:30 +0800

    upgrade

--- src/main/java/com/touchfish/Controller/HistoryController.java
+++ src/main/java/com/touchfish/Controller/HistoryController.java
@@ -14,10 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
