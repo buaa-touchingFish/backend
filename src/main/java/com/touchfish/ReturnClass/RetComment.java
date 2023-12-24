31838cfc19ccb89f02609a1c2ad9e821a8e7e968
unknown-zyz <805290114@qq.com>
2023-12-24 18:34:00 +0800

    Update RetComment.java

--- src/main/java/com/touchfish/ReturnClass/RetComment.java
+++ src/main/java/com/touchfish/ReturnClass/RetComment.java
@@ -15,7 +15,6 @@ public class RetComment {
     private String paper_id;
     private Integer sender_id;
     private String sender_name;
-    private String avatar;
     private LocalDateTime send_time;
     private Integer receiver_id;     //被回复的评论的id，可以为null
     private String avatar = "s5usfv19s.hb-bkt.clouddn.com/OIP-C.jpg"; //头像url地址
